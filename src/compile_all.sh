8577109b95702e5bc61f09c3f0c3836eb6a728d2
Christopher Knight <knightc@jlselogin7.ftm.alcf.anl.gov>
2022-12-03 17:47:20 +0000

    flag for teamsize

--- src/compile_all.sh
+++ src/compile_all.sh
@@ -4,16 +4,21 @@ EXE=jlse_kokkos_sycl_aot
 
 #rm lmp_${EXE}*
 
-for VECLEN in 8 16 32
+TEAM=32
+
+#for VECLEN in 8 16 32
+for VECLEN in 16
 do
   touch kokkos_type.h 
 
-  for TILE in 1 2 4 8 16
+  #for TILE in 1 2 4 8 16
+  #for TILE in 1 2 4 8
   do
     touch pair_snap_kokkos.h 
+    touch pair_snap_kokkos_impl.h
     echo "Compiling VECLEN= ${VECLEN}  TILE= ${TILE}"
 
-    make -j 16 ${EXE} CHRIS_EXTRA="-DCHRIS_VECLEN=${VECLEN} -DCHRIS_TILE=${TILE}" 
+    make -j 16 ${EXE} CHRIS_EXTRA="-DCHRIS_VECLEN=${VECLEN} -DCHRIS_TILE=${TILE} -DCHRIS_TEAM=${TEAM}" 
 
     mv lmp_${EXE} lmp_${EXE}-${VECLEN}-${TILE}
   done
