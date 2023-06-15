b8acfbf35535741b4a907fb09ed7d8b8e1f054ad
Chris Knight <cjknight2009@gmail.com>
2023-06-15 04:27:53 +0000

    cleanup

--- src/compile_one.sh
+++ src/compile_one.sh
@@ -1,3 +1,5 @@
-make -j 1 jlse_kokkos_sycl_aot CHRIS_EXTRA="-DCHRIS_VECLEN=16 -DCHRIS_TILE=2 -DCHRIS_TEAM=32"
-mv lmp_jlse_kokkos_sycl_aot lmp_jlse_kokkos_sycl_aot-16-2
+VERSION=jlse_kokkos_sycl_aot
+#VERSION=jlse_kokkos_sycl_aot_vtune
 
+make -j 32 ${VERSION}
+#make -j 32 ${VERSION} CHRIS_EXTRA="-DCHRIS_VECLEN=16 -DCHRIS_TILE=2 -DCHRIS_TEAM=32 -DCHRIS_TILE_YI=8"
