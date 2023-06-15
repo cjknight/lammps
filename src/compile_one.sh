a178228af1b44210b9bc78a79517acf9fcd8cfca
cjknight <cjknight2009@gmail.com>
2023-06-15 15:51:47 +0000

    polaris configs

--- src/compile_one.sh
+++ src/compile_one.sh
@@ -1,5 +1,7 @@
-VERSION=jlse_kokkos_sycl_aot
+#VERSION=jlse_kokkos_sycl_aot
 #VERSION=jlse_kokkos_sycl_aot_vtune
+#VERSION=polaris_gnu_kokkos
+VERSION=polaris_oneapi_kokkos
 
 make -j 32 ${VERSION}
 #make -j 32 ${VERSION} CHRIS_EXTRA="-DCHRIS_VECLEN=16 -DCHRIS_TILE=2 -DCHRIS_TEAM=32 -DCHRIS_TILE_YI=8"
