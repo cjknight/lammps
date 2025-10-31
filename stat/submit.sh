935f9012c64c4d83a3e31a28845160bf5aeb89fd
Chris Knight <knight@x4219c2s1b0n0.hostmgmt.cm.aurora.alcf.anl.gov>
2025-10-31 19:42:46 +0000

    add lammps example

---
+++ stat/submit.sh
@@ -0,0 +1,42 @@
+#!/bin/bash -l
+#PBS -l select=1024
+#PBS -l place=scatter
+#PBS -l walltime=0:15:00
+#PBS -q prod
+#PBS -A Catalyst
+#PBS -l filesystems=home:flare
+
+cd /flare/catalyst/proj_shared/knight/lammps/aurora_stat/lammps/stat
+
+NNODES=`wc -l < $PBS_NODEFILE`
+NRANKS_PER_NODE=24
+NTHREADS=1
+
+export MPICH_GPU_SUPPORT_ENABLED=1
+export OMP_NUM_THREADS=${NTHREADS}
+export OMP_PLACES=cores
+
+NTOTRANKS=$(( NNODES * NRANKS_PER_NODE ))
+
+EXE=../build/lmp
+
+NSTEPS=10000
+X=2
+Y=2
+Z=2
+
+EXE_ARG="-in in.lj -var nsteps ${NSTEPS} -var x ${X} -var y ${Y} -var z ${Z} "
+EXE_ARG+=" -pk gpu 1 -sf gpu " 
+#EXE_ARG+=" -pk gpu 1 -pk omp ${NTHREADS} -sf hybrid gpu omp " 
+
+MPI_ARG="-n ${NTOTRANKS} --ppn ${NRANKS_PER_NODE} "
+#MPI_ARG+="--cpu-bind list:1:2:3:4:5:6:53:54:55:56:57:58"
+MPI_ARG+="--cpu-bind list:1:2:3:4:5:6:7:8:9:10:11:12:53:54:55:56:57:58:59:60:61:62:63:64"
+#MPI_ARG+="--cpu-bind list:1:2:3:4:5:6:7:8:9:10:11:12:13:14:15:16:17:18:19:20:21:22:23:24:53:54:55:56:57:58:59:60:61:62:63:64:65:66:67:68:69:70:71:72:73:74"
+
+AFFINITY=""
+AFFINITY=" gpu_tile_compact.sh "
+
+COMMAND="mpiexec ${MPI_ARG} ${AFFINITY} ${EXE} ${EXE_ARG}"
+echo "COMMAND= ${COMMAND}"
+${COMMAND}
